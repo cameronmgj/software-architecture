8552c79036a25e9ad527cfbbf1f1217f92cc6a66
cameronmgj <123835556+cameronmgj@users.noreply.github.com>
2023-11-28 21:31:34 +0000

    competitor classes
    created xxxcompetitor class, started initialising code stumps for both competitor classes. added some functionality between functions of the two.

---
+++ pt2/src/CMGJCompetitor.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+public class CMGJCompetitor {
+    private String level;
+    private int competitorNumber;
+    private String country;
+    private int age;
+    private String fname;
+    private String lname;
+    private ArrayList<String> chosenCategories;
+    private ArrayList<Integer> categoryIDs;
+    private ArrayList<Double> scoreList;
+
+    public CMGJCompetitor() {
+
+    }
+
+    public int getID() {
+        return 0;
+    }
+    public int getCompetitorNumber(){
+        return competitorNumber;
+    }
+    public String getCountry() { return country; }
+    public int getAge() { return age; }
+    public String getLevel(){
+        return level;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public String getLname() {
+        return lname;
+    }
+    public String getFullName() {
+        return fname + " " + lname;
+    }
+    public void joinCategory(String cat, int id) {
+        chosenCategories.add(cat);
+        categoryIDs.add(id);
+    }
+    public void removeCategory(String cat) {
+        for (int i=0; i < chosenCategories.size(); i++) {
+            if (chosenCategories.get(i) == cat) {
+                chosenCategories.remove(i);
+                categoryIDs.remove(i);
+            }
+        }
+    }
+    public ArrayList getChosenCategories(){
+        return chosenCategories;
+    }
+    public int getIdForCategory(String cat){
+        return 0;
+    }
+    public void addScore(double s) {
+        scoreList.add(s);
+    }
+    public ArrayList getScores(){
+        return scoreList;
+    }
+}
--- pt2/src/Competitor.java
+++ pt2/src/Competitor.java
@@ -1,17 +1,26 @@
+import java.util.ArrayList;
+
 public class Competitor {
+    private ArrayList<CMGJCompetitor> competitorList;
+
+
     public Competitor() {
 
     }
-    public int getID() {
-        return 0;
+
+    public double getOverallScore(CMGJCompetitor c) {
+        double total = 0;
+        for (int i=0; i < c.getScores().size(); i++) {
+            total += (double) c.getScores().get(i);
+        }
+        return total;
     }
-    public double getOverallScore(){
-        return 5;
+    public String getFullDetails(CMGJCompetitor c) {
+        return "Competitor No: " + c.getCompetitorNumber() + ", Name: " + c.getFullName() + ", Country: " + c.getCountry() + ", Level: " + c.getLevel() + ", Overall Score: " + getOverallScore(c) + ".";
     }
-    public String getFullDetails(){
-        return "";
-    }
-    public String getShortDetails(){
-        return "";
+    public String getShortDetails(CMGJCompetitor c) {
+        String initials = "";
+        initials = initials + c.getFname().charAt(0) + c.getLname().charAt(0);
+        return "CN " + c.getCompetitorNumber() + " (" + initials + ") " + " has overall score " + getOverallScore(c) + ".";
     }
 }
