edfd7255ca7a855e4524149f8fe11979e6f21bcb
cameronmgj <123835556+cameronmgj@users.noreply.github.com>
2023-11-28 15:51:03 +0000

    empty function definitions in Competitor class

--- pt2/src/Competitor.java
+++ pt2/src/Competitor.java
@@ -1,5 +1,17 @@
 public class Competitor {
-    public Competitor(){
+    public Competitor() {
 
+    }
+    public int getID() {
+        return 0;
+    }
+    public double getOverallScore(){
+        return 5;
+    }
+    public String getFullDetails(){
+        return "";
+    }
+    public String getShortDetails(){
+        return "";
     }
 }
